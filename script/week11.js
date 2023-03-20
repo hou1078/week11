14fabf85ce2a2454d3fa97cc0714dfb918e45061
Your Name <you@example.com>
2023-03-20 14:21:33 -0700

    function expression

--- script/week11.js
+++ script/week11.js
@@ -1,4 +1,9 @@
-function greed(name){
+// function greet(name){
+//     console.log("Hello"+" "+ name)
+// }
+// greet("John")
+
+let greet = function(name) {
     console.log("Hello"+" "+ name)
 }
-greed("John")
+greet("John")
